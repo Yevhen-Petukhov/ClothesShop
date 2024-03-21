6fb3326a388c62bb22ddfc0ceed8a80c45886b10
Dmytro <ishchenko44@gmail.com>
2024-03-21 23:36:57 +0200

    Add logs in CalculatorInt

--- src/main/java/org/example/CalculatorInt.java
+++ src/main/java/org/example/CalculatorInt.java
@@ -3,25 +3,30 @@ package org.example;
 public class CalculatorInt implements Calculator<Integer>{
     @Override
     public Integer sum(Integer a, Integer b) {
+        System.out.println("Sum: ");
         return a + b;
     }
     @Override
     public Integer difference(Integer a, Integer b) {
+        System.out.println("Difference: ");
         return a - b;
     }
 
     @Override
     public Integer multiply(Integer a, Integer b) {
+        System.out.println("Multiply: ");
         return a * b;
     }
 
     @Override
     public Integer divide(Integer a, Integer b) {
+        System.out.println("Divide: ");
         return a / b;
     }
 
     @Override
     public Integer square(Integer a) {
+        System.out.println("Square: ");
         return a * a;
     }
 
