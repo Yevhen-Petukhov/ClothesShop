80a392a09bfe0149edf8b53d473553c63e786154
Dmytro <ishchenko44@gmail.com>
2024-03-21 23:32:14 +0200

    Add logs in CalculatorDouble methods

--- src/main/java/org/example/CalculatorDouble.java
+++ src/main/java/org/example/CalculatorDouble.java
@@ -3,25 +3,30 @@ package org.example;
 public class CalculatorDouble implements Calculator<Double> {
     @Override
     public Double sum(Double a, Double b) {
+        System.out.println("Sum: ");
         return a + b;
     }
     @Override
     public Double difference(Double a, Double b) {
+        System.out.println("Difference: ");
         return a - b;
     }
 
     @Override
     public Double multiply(Double a, Double b) {
+        System.out.println("Multiply: ");
         return a * b;
     }
 
     @Override
     public Double divide(Double a, Double b) {
+        System.out.println("Divide: ");
         return a / b;
     }
 
     @Override
     public Double square(Double a) {
+        System.out.println("Square: ");
         return a * a;
     }
 
