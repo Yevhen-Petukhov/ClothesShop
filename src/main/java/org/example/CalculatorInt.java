d5d39b33d6f40fba1561c31ee992d544b2c8c7a6
Dmytro <ishchenko44@gmail.com>
2024-03-21 23:48:27 +0200

    Reset to last commit

--- src/main/java/org/example/CalculatorInt.java
+++ src/main/java/org/example/CalculatorInt.java
@@ -3,38 +3,26 @@ package org.example;
 public class CalculatorInt implements Calculator<Integer>{
     @Override
     public Integer sum(Integer a, Integer b) {
-        System.out.println("Sum: ");
         return a + b;
     }
     @Override
     public Integer difference(Integer a, Integer b) {
-        System.out.println("Difference: ");
         return a - b;
     }
 
     @Override
     public Integer multiply(Integer a, Integer b) {
-        System.out.println("Multiply: ");
         return a * b;
     }
 
     @Override
     public Integer divide(Integer a, Integer b) {
-        System.out.println("Divide: ");
         return a / b;
     }
 
     @Override
     public Integer square(Integer a) {
-        System.out.println("Square: ");
         return a * a;
-    }
-     public Integer findMin(Integer a,Integer b){
-        return Math.min(a,b);
-     }
-
-    public Integer findMax(Integer a,Integer b){
-        return Math.max(a,b);
     }
 
 }
